734a4894c25a7e07801e04bcd1039e4fe4c469c2
jnbk <jbourak@pm.me>
2020-05-06 15:24:11 -0700

    fixed/edited docs and cleaned up code

--- data-raw/vancouver_trees.R
+++ data-raw/vancouver_trees.R
@@ -2,26 +2,17 @@ library(dplyr)
 library(readr)
 library(tidyverse)
 
+src <-
+  "https://opendata.vancouver.ca/explore/dataset/street-trees/download/?format=csv&timezone=America/Los_Angeles&lang=en&use_labels_for_header=true&csv_separator=%3B"
 
-raw <-
-  read_csv2(
-    "https://opendata.vancouver.ca/explore/dataset/street-trees/download/?format=csv&timezone=America/Los_Angeles&lang=en&use_labels_for_header=true&csv_separator=%3B"
-  )
+raw <- read_delim(src, delim = ";")
 
-trees <- raw %>%
-  rename_all(tolower)
+vancouver_trees <- raw %>%
+  rename_all(tolower) %>%
+  mutate(geom = str_replace_all(geom, ".*\\[| |\\].*", "")) %>%
+  separate(geom, c("longitude", "latitude"), sep = ",") %>%
+  mutate(longitude = as.numeric(longitude)) %>%
+  mutate(latitude = as.numeric(latitude))
 
-
-# obtain the latitude and longitude of the column (tried replace and gsub without success)
-trees <- trees %>% separate(geom, c("junk","needed"), sep = "\\[")
-trees <- trees %>% separate(needed, c("needed2","morejunk"), sep = "\\]")
-trees <- trees %>% separate(needed2, c("longitude","latitude"), sep = "\\,")
-trees <- trees %>%  select(-junk, -morejunk) %>%
-                mutate(longitude = as.numeric(longitude),
-                       latitude = as.numeric(latitude))
-vancouver_trees <- trees
-
-#write_csv(vancouver_trees, "data-raw/vancouver_trees.csv")
-save(vancouver_trees, file = "data/vancouver_trees.rda", compress='bzip2')
+save(vancouver_trees, file = "data/vancouver_trees.rda", compress = 'bzip2')
 saveRDS(vancouver_trees, "tests/testthat/vancouver_trees.rds")
-
